439a81828f4484aebc1bc5c1e61bd074ffb99d7c
Jóhann Friðriksson <johannfr@siminn.is>
2014-12-23 14:44:56 +0000

    Initial code commit. PID-Autotune example code compiles.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-fillinn
+Fíllinn
 =======
 
 Kóði fyrir Fílinn
+
+## arduino
+
+PID stýring fyrir Arduino Pro Mini (3v3).
---
+++ arduino/include/main.hpp
@@ -0,0 +1,13 @@
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+
+
+void setup();
+void loop();
+void changeAutoTune();
+void AutoTuneHelper(boolean start);
+void SerialSend();
+void SerialReceive();
+void DoModel();
+
+#endif
