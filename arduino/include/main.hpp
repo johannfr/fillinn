5921b6d084d0d5a6b9fc1ce8a691828e1e4b0f4c
Jóhann Friðriksson <johannfr@siminn.is>
2014-12-29 13:02:14 +0000

    Initial actual version of main. PID code still missing.

--- arduino/include/main.hpp
+++ arduino/include/main.hpp
@@ -3,6 +3,10 @@
 
 void setup();
 void loop();
-void arg_display(int, char**);
+void cmd_setpoint(int, char**);
+void cmd_run(int, char**);
+void cmd_stop(int, char**);
+void cmd_brake(int, char**);
+void cmd_direction(int, char**);
 
 #endif
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -3,55 +3,110 @@
 
 #include <Cmd.h>
 
+
+#define pPWMFront 9
+#define pDirectionFrontA 7
+#define pDirectionFrontB 8
+#define pEncoderFront 4
+
+#define pPWMBack 10
+#define pDirectionBackA 12
+#define pDirectionBackB 11
+#define pEncoderBack 2
+
+
+double setpoint = 0;
+
+bool pidRunning = false;
+
+bool directionA = false;
+bool directionB = false;
+
+
 void setup()
 {
-  // init the command line and set it for a speed of 57600
-  Serial.begin(115200);
-  cmdInit(&Serial);
-  
-  // add the commands to the command table. These functions must
-  // already exist in the sketch. See the functions below. 
-  // The functions need to have the format:
-  //
-  // void func_name(int arg_cnt, char **args)
-  //
-  // arg_cnt is the number of arguments typed into the command line
-  // args is a list of argument strings that were typed into the command line
-  cmdAdd("args", arg_display);
+    Serial.begin(115200);
+    pinMode(pPWMFront, OUTPUT);
+    pinMode(pDirectionFrontA, OUTPUT);
+    pinMode(pDirectionFrontB, OUTPUT);
+    pinMode(pEncoderFront, INPUT);
+    digitalWrite(pEncoderFront, HIGH); // Enable pull-ups
+
+    pinMode(pPWMBack, OUTPUT);
+    pinMode(pDirectionBackA, OUTPUT);
+    pinMode(pDirectionBackB, OUTPUT);
+    pinMode(pEncoderBack, INPUT);
+    digitalWrite(pEncoderBack, HIGH); // Enable pull-ups
+
+
+    cmdInit(&Serial);
+    cmdAdd("setpoint", cmd_setpoint);
 }
 
 void loop()
 {
-  cmdPoll();
+    cmdPoll();
+
+    //TODO Add PID stuff here.
+
+    digitalWrite(pDirectionFrontA, directionA);
+    digitalWrite(pDirectionFrontB, directionB);
+    digitalWrite(pDirectionBackA, !directionA); // Invert, motor is other way around.
+    digitalWrite(pDirectionBackB, !directionB);
+
+
 }
 
-// Example to show what the argument count and arguments look like. The
-// arg_cnt is the number of arguments typed in by the user. "char **args" is 
-// a bit nasty looking, but its a list of the arguments typed in as ASCII strings. 
-// In C, char *something means an array of characters, aka a string. So
-// char **something is an array of an array of characters, or a string array.
-// 
-// Usage: At the command line, type
-// args hello world i love you 3 4 5 yay
-//
-// The output should look like this:
-// Arg 0: args
-// Arg 1: hello
-// Arg 2: world
-// Arg 3: i
-// Arg 4: love
-// Arg 5: you
-// Arg 6: 3
-// Arg 7: 4
-// Arg 8: 5
-// Arg 9: yay
-void arg_display(int arg_cnt, char **args)
+void cmd_setpoint(int arg_cnt, char **args)
 {
-  for (int i=0; i<arg_cnt; i++)
-  {
-    Serial.print("Arg ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(args[i]);
-  }
+
+    if (arg_cnt < 2)
+    {
+        Serial.println(setpoint, DEC);
+        return;
+    }
+
+    setpoint = cmdStr2Num(args[1], 10);
+
+}
+
+
+
+void cmd_run(int arg_cnt, char **args)
+{
+
+    if (arg_cnt < 2)
+    {
+        Serial.println(pidRunning, DEC);
+        return;
+    }
+
+    // TODO Reset PID values.
+    pidRunning = (cmdStr2Num(args[1], 10) == 1 ? true : false);
+}
+
+void cmd_stop(int arg_cnt, char **args)
+{
+    pidRunning = false;
+    directionA = false;
+    directionB = false;
+}
+
+void cmd_brake(int arg_cnt, char **args)
+{
+    pidRunning = false;
+    directionA = true;
+    directionB = true;
+}
+
+void cmd_direction(int arg_cnt, char **args)
+{
+    if (arg_cnt < 2)
+    {
+        Serial.println(directionA, DEC);
+        return;
+    }
+
+    directionA = (cmdStr2Num(args[1], 10) == 1 ? true : false);
+    directionB = !directionA;
 }
